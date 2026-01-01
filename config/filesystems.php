ef85f28071e2319ed795294667bb3c78033bf123
dqaplusapp <dqaplusapp@gmail.com>
2026-01-01 19:39:29 +0100

    Nnukwu ifeffgfddsdusjsks9393 in public33

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,14 +39,11 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public_html'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
-
+    'driver' => 'local',
+    'root' => base_path('../public_html/storage'),
+    'url' => env('APP_URL').'/storage',
+    'visibility' => 'public',
+],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
