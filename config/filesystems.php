7cc59cf17c1148b43dca2531098811076d91758e
dqaplusapp <dqaplusapp@gmail.com>
2026-01-01 19:35:54 +0100

    Nnukwu ifeffgfddsdusjsks9393 in public

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/public_html'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
